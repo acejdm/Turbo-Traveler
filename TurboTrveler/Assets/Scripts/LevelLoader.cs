6278c84bc3bc1676fcf258a77eae9675116861f1
acejdm <aaron_robley@bloomfield.edu>
2020-10-12 22:44:14 -0400

    Added title screen and settings menu

---
+++ TurboTrveler/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    GameObject menu_Canvas;
+    GameObject settings_Canvas;
+
+    void Start()
+    {
+        menu_Canvas = GameObject.Find("MainMenu_Canvas");
+        settings_Canvas = GameObject.Find("Settings_Canvas");
+
+        settings_Canvas.SetActive(false);
+        menu_Canvas.SetActive(true);
+    }
+
+    public void LoadLevel (int a)
+    {
+        Application.LoadLevel(a);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMenu ()
+    {
+        menu_Canvas.SetActive(true);
+        settings_Canvas.SetActive(false);
+    }
+
+    public void LoadSettings()
+    {
+        menu_Canvas.SetActive(false);
+        settings_Canvas.SetActive(true);
+    }
+}
