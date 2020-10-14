e2517430839e3bc0e57d3facf4f894848b8cee59
acejdm <aaron_robley@bloomfield.edu>
2020-10-14 01:52:48 -0400

    Car Selection Screen

---
+++ TurboTrveler/Assets/Scripts/CarSelector.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelector : MonoBehaviour
+{
+
+    public GameObject blueCar;
+    public GameObject redCar;
+    public GameObject grayCar;
+
+    public int carSelected;
+
+    void Start()
+    {
+        blueCar.SetActive(true);
+        redCar.SetActive(false);
+        grayCar.SetActive(false);
+
+        carSelected = 1;
+    }
+
+    public void loadBlue()
+    {
+        blueCar.SetActive(true);
+        redCar.SetActive(false);
+        grayCar.SetActive(false);
+
+        carSelected = 1;
+    }
+
+    public void loadRed ()
+    {
+        blueCar.SetActive(false);
+        redCar.SetActive(true);
+        grayCar.SetActive(false);
+
+        carSelected = 2;
+    }
+
+    public void loadGray ()
+    {
+        blueCar.SetActive(false);
+        redCar.SetActive(false);
+        grayCar.SetActive(true);
+
+        carSelected = 3;
+    }
+}
