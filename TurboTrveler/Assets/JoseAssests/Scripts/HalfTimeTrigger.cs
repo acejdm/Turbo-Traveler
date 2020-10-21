767a1fc3406d67cec9c927a1ff6db9a873d33295
josemg2556 <56130895+josemg2556@users.noreply.github.com>
2020-10-20 23:47:16 -0400

    New Stuff
    Timer added

---
+++ TurboTrveler/Assets/JoseAssests/Scripts/HalfTimeTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HalfTimeTrigger : MonoBehaviour
+{
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTrigger()
+    {
+        LapCompleteTrig.SetActive(true);
+        HalfLapTrig.SetActive(false);
+    }
+}
