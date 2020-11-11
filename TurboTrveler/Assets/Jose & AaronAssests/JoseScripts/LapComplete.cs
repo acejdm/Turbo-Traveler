3fcf3f666f7da689e684f9de05c500d031a6f954
josemg2556 <56130895+josemg2556@users.noreply.github.com>
2020-11-10 23:42:37 -0500

    New Features
    Mini Map
    Race Position
    lap count
    best time
    there still seems to be a problem when it comes to adding the best time

--- TurboTrveler/Assets/Jose & AaronAssests/JoseScripts/LapComplete.cs
+++ TurboTrveler/Assets/Jose & AaronAssests/JoseScripts/LapComplete.cs
@@ -29,41 +29,51 @@ public class LapComplete : MonoBehaviour
         {
             RaceFinish.SetActive(true);
         }
+
+        if(LapsDone == 2)
+        {
+            RaceFinish.SetActive(true);
+        }
+       
     }
 
     void OnTriggerEnter()
     {
         LapsDone += 1;
+       
         RawTime = PlayerPrefs.GetFloat("RawTime");
-        if(LapTimeManeger.RawTime <= RawTime)
-        if(LapTimeManeger.SecondCount <= 9)
+        if (LapTimeManeger.RawTime <= RawTime)
         {
-            SecondDisplay.GetComponent<Text> ().text = "0" + LapTimeManeger.SecondCount + ".";
+            if (LapTimeManeger.SecondCount <= 9)
+            {
+                SecondDisplay.GetComponent<Text>().text = "0" + LapTimeManeger.SecondCount + ".";
 
-        }
-        else
-        {
-            SecondDisplay.GetComponent<Text>().text = "" + LapTimeManeger.SecondCount + "."; 
-        }
-        if (LapTimeManeger.MinuteCount <= 9)
-        {
-            MinuteDisplay.GetComponent<Text>().text = "0" + LapTimeManeger.MinuteCount + ".";
+            }
+            else
+            {
+                SecondDisplay.GetComponent<Text>().text = "" + LapTimeManeger.SecondCount + ".";
+            }
+            if (LapTimeManeger.MinuteCount <= 9)
+            {
+                MinuteDisplay.GetComponent<Text>().text = "0" + LapTimeManeger.MinuteCount + ".";
 
-        }
-        else
-        {
-            MinuteDisplay.GetComponent<Text>().text = "" + LapTimeManeger.MinuteCount + ".";
-        }
+            }
+            else
+            {
+                MinuteDisplay.GetComponent<Text>().text = "" + LapTimeManeger.MinuteCount + ".";
+            }
 
-        MilliDisplay.GetComponent<Text>().text = "" + LapTimeManeger.MilliCount;
-
+            MilliDisplay.GetComponent<Text>().text = "" + LapTimeManeger.MilliCount;
+        }
         PlayerPrefs.SetInt(" MinSave", LapTimeManeger.MinuteCount);
         PlayerPrefs.SetInt(" SecSave", LapTimeManeger.SecondCount);
         PlayerPrefs.SetFloat(" Milli Save", LapTimeManeger.MilliCount);
+        PlayerPrefs.SetFloat("RawTime", LapTimeManeger.RawTime);
 
         LapTimeManeger.MinuteCount = 0;
         LapTimeManeger.SecondCount = 0;
         LapTimeManeger.MilliCount = 0;
+        LapTimeManeger.RawTime = 0;
         LapCounter.GetComponent<Text>().text = "" + LapsDone;
         HalfLapTrig.SetActive(true);
         LapCompleteTrig.SetActive(false);
